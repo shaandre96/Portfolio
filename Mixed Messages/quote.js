97b00983f5ef98c6e1aa1d2ec02ab6e90728e839
shaandre96 <68268791+shaandre96@users.noreply.github.com>
2022-01-13 14:47:04 +1100

    random quote selection and console display

--- Mixed Messages/quote.js
+++ Mixed Messages/quote.js
@@ -5,7 +5,15 @@ const api_url = "https://zenquotes.io/api/quotes";
 async function getapi(url){
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data);
-};
+    return data;
+    };
 
-getapi(api_url);
+var quotes = await getapi(api_url);
+
+const displayRandomQuote = arr =>{
+    var randIndex = Math.floor(Math.random() * arr.length-1);
+    var obj = arr[randIndex];
+    console.log(`${obj.q} \n - ${obj.a}`);
+}
+
+displayRandomQuote(quotes);

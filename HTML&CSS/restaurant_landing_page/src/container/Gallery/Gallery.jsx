d2c502355299957ea9864de09dd5791babc1fd7a
shaandre96 <68268791+shaandre96@users.noreply.github.com>
2022-02-03 15:07:57 +1100

    implement scroll function

--- HTML&CSS/restaurant_landing_page/src/container/Gallery/Gallery.jsx
+++ HTML&CSS/restaurant_landing_page/src/container/Gallery/Gallery.jsx
@@ -5,13 +5,33 @@ import {images} from '../../constants';
 import './Gallery.css';
 
 const Gallery = () => {
- 
- 
+  const scrollRef = React.useRef(null);
+  const scroll = (direction) => {
+    const {current} = scrollRef;
+    if(direction==='left'){
+      current.scrollLeft -= 300;
+    }else if (direction==='right'){
+      current.scrollLeft += 300;
+    }
+  }
   return(
     <div className='app__gallery flex__center'>
       <div className='app__gallery-content'>
         <SubHeading title="Instagram"/>
-        
+        <h1 className='headtext__cormorant'>Photo Gallery</h1>
+        <p className='p__opensans' style={{color: '#AAA', marginTop: '2rem'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam facilisis, magna dictum sodales maximus, lorem massa congue tellus, a volutpat urna nisl at enim. Cras nec interdum quam, id egestas ipsum.</p>
+        <button type='button' className='custom__button'>View More</button>;
+      </div>
+    
+
+      <div className='app__gallery-images'>
+        <div className='app__gallery-images_container' ref={scrollRef}>
+
+        </div>
+        <div className='app__gallery-images_arrows'>
+          <BsArrowLeftShort className='gallery__arrow-icon' onClick={()=> scroll('left')}/>
+          <BsArrowRightShort className='gallery__arrow-icon' onClick={()=> scroll('right')}/>
+        </div>
       </div>
     </div>
   );  

913237ffc90478b28196a205c3e7df3418746ae3
shaandre96 <68268791+shaandre96@users.noreply.github.com>
2022-02-07 10:30:12 +1100

    hero styling and email link

--- Website/src/components/Hero/Hero.js
+++ Website/src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        I'm Andre Sha, <br/>
+        A creative <br/>
+        Software Engineer
+      </SectionTitle> 
+      <SectionText>
+        My main experience in is desktop app development and font-end web development. But I always look forward to exciting opportunities, love learning and developing new skills. 
+      </SectionText>
+      <Button onclick={()=>window.location.href="mailto:shaandre96@gmail.com"}>Reach Out</Button> 
+    </LeftSection>  
+  </Section>
 );
 
 export default Hero;

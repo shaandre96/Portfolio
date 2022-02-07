d59cd4070fde435d58494f64fe2efd56f8738f14
shaandre96 <68268791+shaandre96@users.noreply.github.com>
2022-02-07 17:03:37 +1100

    add contacts

--- Website/src/components/Footer/Footer.js
+++ Website/src/components/Footer/Footer.js
@@ -6,9 +6,31 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:+61430492266'>0430 492 266</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='mailto:shaandre96@gmail.com'>shaandre96@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>
+          Pride makes us artificial and humility makes us real. Growth through learning.
+          </Slogan>
+        </CompanyContainer>
+        <SocialIcons href='https://github.com/shaandre96/Portfolio' target='_blank'>
+          <AiFillGithub size='3rem'/>
+        </SocialIcons>
+        <SocialIcons href='https://www.linkedin.com/in/andre-sha-744a65226/' target='_blank'>
+          <AiFillLinkedin size='3rem'/>
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 

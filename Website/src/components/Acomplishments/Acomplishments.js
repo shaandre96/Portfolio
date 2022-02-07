23483a9947413c70db7bf770b64e3b4142c8b614
shaandre96 <68268791+shaandre96@users.noreply.github.com>
2022-02-07 16:54:27 +1100

    styling

--- Website/src/components/Acomplishments/Acomplishments.js
+++ Website/src/components/Acomplishments/Acomplishments.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import {Data} from '../../constants/constants'
+import { CardInfo } from '../Projects/ProjectsStyles';
 
-const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
-];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>Personal Acomplishments</SectionTitle>
+    <Boxes>
+      {Data.map((card, index)=>(
+        <Box key={index}>
+          <BoxNum>{card.number}+</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;

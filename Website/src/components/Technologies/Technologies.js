7e7726712eab9f18db7b0cb8291374903f650a66
shaandre96 <68268791+shaandre96@users.noreply.github.com>
2022-02-07 11:20:09 +1100

    technologies section styling

--- Website/src/components/Technologies/Technologies.js
+++ Website/src/components/Technologies/Technologies.js
@@ -4,9 +4,45 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    <SectionDivider/>
+    <SectionTitle main>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies in the web development ecosystem. From front-end to back-end, I am interested in all technology stacks.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size='3rem'/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size='3rem'/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Node and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size='3rem'/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            tools like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;

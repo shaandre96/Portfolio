8ce0a9c40ed3d3c76d624f44f6145054f990b62e
shaandre96 <68268791+shaandre96@users.noreply.github.com>
2022-08-18 19:48:04 +1000

    create react fitness app

---
+++ REACT/work-out-app/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { Box } from '@mui/material';
+
+import  HeroBanner  from '../components/HeroBanner.js';
+import  SearchExercises  from '../components/SearchExercises.js';
+import  Exercises  from '../components/Exercises.js';
+const Home = () => {
+    const [exercises, setExercises] = useState([]);
+    const [bodyPart, setBodyPart] = useState('all');
+
+
+    return (
+        <Box>
+            <HeroBanner />
+            <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+            <Exercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        </Box>
+)
+}
+
+export default Home

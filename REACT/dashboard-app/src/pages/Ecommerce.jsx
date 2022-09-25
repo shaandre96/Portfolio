1f8b750893e760276a34e4292354fe42f30e851b
shaandre96 <68268791+shaandre96@users.noreply.github.com>
2022-09-25 21:16:02 +1000

    add new components and pages

--- REACT/dashboard-app/src/pages/Ecommerce.jsx
+++ REACT/dashboard-app/src/pages/Ecommerce.jsx
@@ -47,6 +47,52 @@ const Ecommerce = () => {
                   <span><GoPrimitiveDot /></span>
                   <span>Expense</span>
                 </p>
+                <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
+                  <span><GoPrimitiveDot /></span>
+                  <span>Budget</span>
+                </p>
+              </div>
+            </div>
+            <div className='empty-10 flex gap-10 flex-wrap justify-center'>
+              <div className='border-r-1 border-color m-4 pr-10'>
+                <div>
+                  <p>
+                    <span className='text-3xl font-semibold'>$92,506</span>
+                    <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>22%</span>
+                  </p>
+                  <p className='text-gray-500 mt-1'>Budget</p>
+                </div>
+                <div className='mt-8'>
+                  <p>
+                    <span className='text-3xl font-semibold'>$47,236</span>
+                  </p>
+                  <p className='text-gray-500 mt-1'>Expense</p>
+                </div>
+                <div className='mt-5'>
+                  <SparkLine 
+                    currentColor='blue'
+                    id='line-sparkLine'
+                    type='Line'
+                    height='80px'
+                    width='250px'
+                    data={SparklineAreaData}
+                    color='blue'
+                  />
+                </div>
+                <div className='mt-10'>
+                  <Button 
+                  color='white'
+                  bgColor='blue'
+                  text='Download Report'
+                  borderRadius='10px'
+                  />
+                </div>
+              </div>
+              <div>
+                <Stacked 
+                  width='320px'
+                  height='360px'
+                />
               </div>
             </div>
           </div>

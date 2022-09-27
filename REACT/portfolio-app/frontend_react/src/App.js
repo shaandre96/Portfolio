9fffb2309e43c605dcb3e43bd8d7aabbc89a83e5
shaandre96 <68268791+shaandre96@users.noreply.github.com>
2022-09-27 20:41:57 +1000

    set up assets and constants

--- REACT/portfolio-app/frontend_react/src/App.js
+++ REACT/portfolio-app/frontend_react/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { About, Footer, Header, Skills, Testimonials, Work } from './containers'
+import { Navbar } from './components'
+import './App.scss'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonials />
+        <Footer />
+    </div>
   )
 }
 

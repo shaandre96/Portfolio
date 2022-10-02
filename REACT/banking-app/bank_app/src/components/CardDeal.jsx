e2ace30fe55f2db910af4188df1b75aa24744866
shaandre96 <68268791+shaandre96@users.noreply.github.com>
2022-10-02 17:10:12 +1100

    billing and card deals

--- REACT/banking-app/bank_app/src/components/CardDeal.jsx
+++ REACT/banking-app/bank_app/src/components/CardDeal.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, {layout} from '../styles'
+import Button from './Button'
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}> 
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden' /> in a few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With ten cards to choose from, it’s easy to find a card to suit your lifestyle and goals.</p>
+        <Button styles='m-10 rounded-[10px]' />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt='card' className='w-[100%] h-[100%]' />
+      </div>
+    </section>
   )
 }
 
